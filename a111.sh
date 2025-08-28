7c9b07ba6d74cea6d585b94b2b85dbbf18849fa3
feeelsbatata <30759166+feeelsbatata@users.noreply.github.com>
2025-08-28 18:32:53 -0300

    Update a111.sh

--- a111.sh
+++ a111.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
 A1111_DIR=${WORKSPACE}/stable-diffusion-webui
+
+mkdir -p "$A1111_DIR/models/Stable-diffusion"
+mkdir -p "$A1111_DIR/models/Lora"
+mkdir -p "$A1111_DIR/models/VAE"
+
 # Checkpoint
-wget -O "$A1111_DIR/models/Stable-diffusion/model.safetensors" \
+wget -O "$A1111_DIR/models/Stable-diffusion/prefectIllustriousXL_v3.safetensors" \
   "https://civitai.com/api/download/models/2081643?type=Model&format=SafeTensor&size=pruned&fp=fp16&token=$CIVITAI_TOKEN"
 
 # Loras
-wget -O "$A1111_DIR/models/Lora/lora1.safetensors" \
+wget -O "$A1111_DIR/models/Lora/M1nma_style-000018.safetensors" \
   "https://civitai.com/api/download/models/1604190?type=Model&format=SafeTensor&token=$CIVITAI_TOKEN"
 
-wget -O "$A1111_DIR/models/Lora/lora2.safetensors" \
+wget -O "$A1111_DIR/models/Lora/Semi-Realism_Illustrious.safetensors" \
   "https://civitai.com/api/download/models/1340291?type=Model&format=SafeTensor&token=$CIVITAI_TOKEN"
 
-wget -O "$A1111_DIR/models/Lora/lora3.safetensors" \
+wget -O "$A1111_DIR/models/Lora/[Style] Daily Lives of my Countryside v2 [Illustrious-XL 2.0].safetensors" \
   "https://civitai.com/api/download/models/1774470?type=Model&format=SafeTensor&token=$CIVITAI_TOKEN"
 
 # VAE
-wget -O "$A1111_DIR/models/VAE/vae.safetensors" \
+wget -O "$A1111_DIR/models/VAE/sdxlVAE_sdxlVAE.safetensors" \
   "https://civitai.com/api/download/models/333245?type=Model&format=SafeTensor&token=$CIVITAI_TOKEN"
